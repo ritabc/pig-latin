ff19227deb80ca6d2400a7d089f5701e330fcefb
Nicholas Brown and Rita Bennett-Chew <rita@>
2018-06-06 14:35:33 -0700

    Add functionality to check for word starting with vowel or consonant, and return word as appropriate. Does not work for words starting with 2 or 2+ consonants

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
-var ending = "ay"
 
 $(document).ready(function() {
   $("#submit").click(function(event){
     event.preventDefault();
     var input = $("#input").val();
     if (!hasNumber(input)) {
-      var toPrint = isTheFirstLetterAVowel(input)
+      var toPrint = moveOneBeginningConsonant(input)
     }
     console.log(toPrint)
+    // moveOneBeginningConsonant(input);
   });
 });
 
@@ -25,15 +25,39 @@ function hasNumber(input){
   }
 };
 
- function isTheFirstLetterAVowel(word) {
-   vowelList=["a", "e", "i", "o", "u"];
-   for (y=0;y<=vowelList.length -1;y+=1){
-     if (word[0] == vowelList[y]){
-       return true
-     }
-   }
-   y+=1
-   if (y==6){
-     return false
+function isTheFirstLetterAVowel(word) {
+ vowelList=["a", "e", "i", "o", "u"];
+ for (y=0;y<=vowelList.length -1;y+=1){
+   if (word[0] == vowelList[y]){
+     return true
    }
  }
+ y+=1
+ if (y==6){
+   return false
+ }
+}
+
+// function isVowel(char) {
+//    vowelList=["a", "e", "i", "o", "u"];
+//    for (y = 0; y <= vowelList)
+// }
+function addAYEnding(word) {
+ return word + "ay"
+}
+
+function vowelBeginningAddEnding(word) {
+ // var input = $("#input").val();
+ if (isTheFirstLetterAVowel(word)) {
+   return addAYEnding(word);
+ }
+}
+
+function moveOneBeginningConsonant(word) {
+  if (!isTheFirstLetterAVowel(word)) {
+    var firstLetter = word.slice(0, 1);
+    var endOfWord = word.slice(1);
+    var scrambled = endOfWord + firstLetter;
+    return addAYEnding(scrambled);
+  }
+}
