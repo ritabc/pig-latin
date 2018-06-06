dd37cba2ddbafe69111a6024795a66d9ff16d1b5
Nicholas Brown and Rita Bennett-Chew <rita@>
2018-06-06 11:41:59 -0700

    Add hasNumber function to check whether the input has any numbers

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,21 @@ $(document).ready(function() {
   $("#submit").click(function(event){
     event.preventDefault();
     var input = $("#input").val();
-    console.log(input);
-  })
-})
+    // console.log(input);
+    var toPrint = hasNumber(input)
+    console.log(toPrint)
+  });
+});
+
+function hasNumber(input){
+  for (iterator = 0; iterator <= input.length - 1; iterator++) {
+    for (number = 0; number <= 9; number++) {
+      if (input[iterator] == number) {
+        return true // if it has a number
+      }
+    }
+  }
+  if (iterator == input.length){
+    return false
+  }
+};
