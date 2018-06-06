de3730cb0e605c399cbaec4f25ab61ce58f25038
Nicholas Brown and Rita Bennett-Chew <nicholas@>
2018-06-06 13:37:56 -0700

    return true if first letter is a vowel, and false if its a consonant

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,12 @@
+var ending = "ay"
+
 $(document).ready(function() {
   $("#submit").click(function(event){
     event.preventDefault();
     var input = $("#input").val();
-    // console.log(input);
-    var toPrint = hasNumber(input)
+    if (!hasNumber(input)) {
+      var toPrint = isTheFirstLetterAVowel(input)
+    }
     console.log(toPrint)
   });
 });
@@ -12,6 +15,7 @@ function hasNumber(input){
   for (iterator = 0; iterator <= input.length - 1; iterator++) {
     for (number = 0; number <= 9; number++) {
       if (input[iterator] == number) {
+        alert("Please enter a word or sentence without number digits");
         return true // if it has a number
       }
     }
@@ -20,3 +24,16 @@ function hasNumber(input){
     return false
   }
 };
+
+ function isTheFirstLetterAVowel(word) {
+   vowelList=["a", "e", "i", "o", "u"];
+   for (y=0;y<=vowelList.length -1;y+=1){
+     if (word[0] == vowelList[y]){
+       return true
+     }
+   }
+   y+=1
+   if (y==6){
+     return false
+   }
+ }
